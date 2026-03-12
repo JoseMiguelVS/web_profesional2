9469eafb42743900c94ef1ef66642034b4e6a2ea
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2026-03-12 11:04:06 -0600

    Agregar imagenes

--- app.js
+++ app.js
@@ -7,3 +7,33 @@ const data = [
   { id: "p06", title: "Ruta", desc: "Camino en perspectiva", src: "https://picsum.photos/id/1005/1200/675" }
 ];
 
+//Recuperar elementos del DOM
+const thumbs = document.querySelector("#thumbs");
+const heroImg = document.querySelector("#heroImg");
+const heroTitle = document.querySelector("#heroTitle");
+const heroDesc = document.querySelector("#heroDesc");
+const counter = document.querySelector("#counter");
+
+const likeBtn = document.querySelector("#likeBtn");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+const playBtn = document.querySelector("#playBtn");
+
+//Trbajar con el estado de la aplicación
+let currentIndex = 0; //Indice de la imagen actual
+const likes = {}; //Objeto para almacenar los estados de "me gusta" por imagen
+
+//Renderizar la gelería de miniaturas
+function renderThumbs() {
+  thumbs.innerHTML = data.map((item, index) => {
+    return `
+      <article class="thumb ${index === currentIndex ? "active" : ""}" data-index="${index}">
+        <span class="badge">${index + 1}</span>
+        <img src="${item.src}" alt="${item.title}" />
+      </article>
+    `;
+  }).join("");
+};
+
+//Renderizar la imagen principal
+renderThumbs();
